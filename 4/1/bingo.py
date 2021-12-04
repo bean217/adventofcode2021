afa577e291ad676081926ac7257c776f3105276f
Benjamin  PIRO <benjamin.piro@carestream.com>
2021-12-04 17:50:34 -0500

    started day 4

---
+++ 4/1/bingo.py
@@ -0,0 +1,56 @@
+class Bingo:
+    def __init__(self):
+        self.num_seq = []
+        self.boards = []
+
+    def set_num_seq(self, num_seq):
+        self.num_seq = num_seq
+
+    def add_board(self, board):
+        self.boards += [board]
+
+class Board:
+    def __init__(self):
+        self.board = [[0]*5]*5
+
+    def __str__(self):
+        result = ""
+        for j in range(5):
+            for i in range(5):
+                if (self.board[i][j] < 10):
+                    result += " "
+                result += f'{str(self.board[j][i])}'
+                if (i != 4):
+                    result += " "
+            result += "\n"
+        return result
+
+def main():
+    with open('test') as f:
+        # first line is the game number sequence
+        bingo = Bingo()
+        for line in f:
+            bingo.set_num_seq([int(i) for i in line.strip().split(',')])
+            break
+        print(bingo.num_seq)
+        # Create boards
+        board = Board()
+        row_num = 0
+        for line in f:
+            line = line.strip()
+            if line == "":
+                # new board
+                board = Board()
+                row_num = 0
+                continue
+            line = [int(i) for i in line.split()]
+            for i in range(5):
+                board.board[row_num][i] = line[i]
+            bingo.add_board(board)
+            row_num += 1
+        for b in bingo.boards:
+            print(b)
+
+
+if __name__ == "__main__":
+    main()
