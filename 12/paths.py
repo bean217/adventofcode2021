dfca43e71db016d366708a7906ca1c84616d3005
brp8396 <brp8396@rit.edu>
2021-12-17 18:07:27 -0500

    Completed day 12 part 1

--- 12/paths.py
+++ 12/paths.py
@@ -6,16 +6,34 @@ def display(graph):
         print(f'{key}: {graph[key]}')
 
 
+def copy(arr):
+    a_copy = []
+    for elem in arr:
+            a_copy += [elem]
+    return a_copy
+
+
 def is_small_cave(cave_str):
     return ord(cave_str[0]) >= ord('a') and ord(cave_str[0]) <= ord('z')
 
 
 def evaluate_paths(graph, stack, visited=[], curr_path=[], paths=[]):
-    print(f'visited: {visited}')
+    temp_stack = []
     curr = stack.pop()
     curr_path += [curr]
     if is_small_cave(curr):
         visited += [curr]
+    if curr == "end":
+        #print(curr_path)
+        paths += [curr_path]
+        return None
+    for node in graph[curr]:
+        if node not in visited:
+            temp_stack += [node]
+            #print("added")
+            evaluate_paths(graph, temp_stack, copy(visited), copy(curr_path), paths)
+    return paths
+
 
 def construct_graph(fname):
     graph = dict()
@@ -26,17 +44,26 @@ def construct_graph(fname):
                 graph[line[0]] = []
             if line[1] not in graph.keys():
                 graph[line[1]] = []
-            graph[line[0]] += [line[1]]
-            graph[line[1]] += [line[0]]
+            if line[1] != "start" and line[0] != "end":
+                graph[line[0]] += [line[1]]
+            if line[0] != "start" and line[1] != "end":
+                graph[line[1]] += [line[0]]
     return graph
 
 def main():
-    fname = 'test1'
+    fname = 'input'
     graph = construct_graph(fname)
     display(graph)
     stack = []
     stack.append("start")
     paths = evaluate_paths(graph, stack)
-    print(paths)
+    for path in paths:
+        if len(path) > 7:
+            print(path[:7] + ['...'])
+        else:
+            print(path)
+    print(f'{len(paths)} paths.')
+
+
 if __name__ == "__main__":
     main()
