e138fc2c55300f11b8216b51639d6a4868fcbe3e
brp8396 <brp8396@rit.edu>
2021-12-19 15:38:59 -0500

    Implemented minheap for use in dijkstras for day 15

---
+++ 15/chitons.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+        self.size = 0
+
+    def add(self, val):
+        self.size += 1
+        self.heap += [val]
+        index = self.size - 1
+        if index != 0:
+            while self.heap[(index-1)//2] > val:
+                temp = self.heap[(index-1)//2]
+                self.heap[(index-1)//2] = val
+                self.heap[index] = temp
+                index = (index-1)//2
+                if index == 0:
+                    break
+
+    def remove(self):
+        if self.size > 0:
+            self.size -= 1
+            val = self.heap[0]
+            bottom = self.heap.pop()
+            if self.size > 0:
+                self.heap[0] = bottom
+                index = 0
+                while index < self.size:
+                    print(index)
+                    i_temp = 0
+
+                    left_val = None
+                    right_val = None
+
+                    if (index*2)+1 < self.size:
+                        left_val = self.heap[(index*2)+1]
+                    if (index*2)+2 < self.size:
+                        right_val = self.heap[(index*2)+2]
+
+                    if left_val == None and right_val == None:
+                        break
+                    elif right_val == None:
+                        i_temp = (index*2)+1 
+                    else:
+                        if left_val < right_val:
+                            i_temp = (index*2)+1
+                        else:
+                            i_temp = (index*2)+2
+                    if self.heap[index] > self.heap[i_temp]:
+                        temp = self.heap[index]
+                        self.heap[index] = self.heap[i_temp]
+                        self.heap[i_temp] = temp
+                        index = i_temp
+                    else:
+                        break        
+            return val
+
+    def __str__(self):
+        return self.heap.__str__()
+
+class Graph:
+    def __init__(self, fname):
+        self.graph = []
+        with open(fname) as f:
+            for line in f:
+                self.graph += [[int(val) for val in line.strip()]]
+        self.visited
+
+def read_graph(fname):
+    graph = []
+    with open(fname) as f:
+        for line in f:
+            graph += [[int(val) for val in line.strip()]]
+    return graph
+
+
+def main():
+    fname = 'test'
+    graph = read_graph(fname)
+    #for row in graph:
+    #    print(row)
+    heap = MinHeap()
+    print(heap)
+    heap.add(5)
+    print(heap)
+    heap.add(6)
+    print(heap)
+    heap.add(1)
+    print(heap)
+    heap.add(0)
+    print(heap)
+    while heap.size > 0:
+        heap.remove()
+        print(heap)
+    heap.remove()
+    print(heap)
+if __name__ == "__main__":
+    main()
